93e343683cc6d22f5862b2c53e96f5d3dd0d7231
nesskyo <kazukirin64@gmail.com>
2026-09-15 12:55:50 +0800

    error fixed

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "*.placehold.co",
+      },
+      {
+        protocol: "https",
         hostname: "*.supabase.co",
       },
     ],
